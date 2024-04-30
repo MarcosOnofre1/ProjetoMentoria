497c782d594601d92bd240f2a8c1f919a0b7de2a
Marcos Onofre <marcosxrzz@gmail.com>
2024-04-29 22:37:53 -0300

    :tada:	init project work in progress

---
+++ app/src/main/java/com/example/projetomentoria/SecondFragment.kt
@@ -0,0 +1,43 @@
+package com.example.projetomentoria
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.projetomentoria.databinding.FragmentFirstBinding
+import com.example.projetomentoria.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNextPage2.setOnClickListener{ onBtnNextPageClicked() }
+
+    }
+
+    private fun onBtnNextPageClicked() {
+        findNavController().navigate(R.id.thirdFragment)
+
+    }
+}
---
+++ app/src/main/java/com/example/projetomentoria/ThirdFragment.kt
@@ -0,0 +1,41 @@
+package com.example.projetomentoria
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.projetomentoria.databinding.FragmentFirstBinding
+import com.example.projetomentoria.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: ThirdFragmentArgs by navArgs()
+
+    private var name = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        name = args.name
+//
+//        binding.apply {
+//           textThirdPageName.text = name
+//        }
+    }
+
+}
